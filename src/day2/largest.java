6c1eea6930963985a27f5d840716e583e82f2f56
anand162000 <ananthakrishnanvs162000@gmil.com>
2022-02-03 11:49:57 +0530

    first commit

---
+++ src/day2/largest.java
@@ -0,0 +1,19 @@
+package day2;
+import java.util.*;
+public class largest {
+
+	public static void main(String [] args)
+	{
+		int [] a = new int[10];
+		int m=0;
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<10;i++)
+			a[i] = sc.nextInt();
+		for(int i=0; i<10;i++)
+		{
+			if(a[i] > m)
+				m=a[i];
+		}
+		System.out.println("Largest number =" + m);
+	}
+}
